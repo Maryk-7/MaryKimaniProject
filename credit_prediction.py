fb2564706758eb3a431258f2a84cdf4ea8b36faa
Maryk-7 <102730567+Maryk-7@users.noreply.github.com>
2023-02-28 22:31:00 +0300

    Add files via upload

--- credit_prediction.py
+++ credit_prediction.py
@@ -3,7 +3,7 @@ from PIL import Image
 import pickle
 
 
-model = pickle.load(open('./Model/ML_Model.pkl', 'rb'))
+model = pickle.load(open('Model/ML_Model.pkl', 'rb'))
 
 def run():
     st.title("Credit Risk Assessment App")
